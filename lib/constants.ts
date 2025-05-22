0ed026903aa236dbaceed874035d53321d8ea1d6
Temesgen Birechis <birechis@gmail.com>
2025-05-22 11:34:46 +0300

    Added Header Component

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'BNILE'
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'High value for less price'
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTIOMN || 'An Amazon clone with Next.js and MongoDB'
