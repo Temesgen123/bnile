3884a74be1fd160e3bf04145fbc3a7231cd0cf7c
Temesgen Birechis <birechis@gmail.com>
2025-07-22 15:21:43 +0300

    ...

--- app/(root)/account/orders/page.tsx
+++ app/(root)/account/orders/page.tsx
@@ -61,7 +61,8 @@ export default async function OrderPage(props: {
             {orders?.data?.length !== 0 && (
               <TableRow>
                 <TableCell colSpan={6} className="">
-                  You have orders but I couldn&apos;t display it.
+                  You have orders but I couldn&apos;t display it. The length of
+                  ordders is : `${orders.data.length}`
                 </TableCell>
               </TableRow>
             )}
